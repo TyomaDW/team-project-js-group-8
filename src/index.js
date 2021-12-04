c2ded615bc77809f331c047f9a860366f7bf4ecf
Valentin Sachenko <valentin181988@gmail.com>
2021-12-04 13:17:22 +0300

    last

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-const modal = document.getElementById("myModal");
+/* const modal = document.getElementById("myModal");
 const btn = document.getElementById("myBtn");
 const button = document.getElementsByClassName("close")[0];
 
@@ -12,11 +12,23 @@ button.onclick = function() {
 }
 
 window.onclick = function (event) {
-  console.log(event.target)
   if (event.target == modal) {
     modal.style.display = "none";
   }
+} */
+
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
 }
-
-
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
 
