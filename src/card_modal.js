a6498ac1d57861e98c49674a35ad41c0d44d5903
Valentin Sachenko <valentin181988@gmail.com>
2021-12-04 17:51:20 +0300

    last

---
+++ src/card_modal.js
@@ -0,0 +1,15 @@
+const modalBtn = document.getElementById("modal-btn")
+const modal = document.querySelector(".modal")
+const closeBtn = document.querySelector(".close-btn")
+
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
