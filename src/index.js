afc19a9f26b1c71aceddb8564d3447c7d70efd1c
Irchik512 <chornairchik@gmail.com>
2021-12-09 03:36:37 +0200

    add snow decoration
    snow no-stop,
    +clean common

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import './js/card_modal';
 import { refs, renderGallery } from './js/renderGallery';
 import './js/footer_modal';
 import './js/renderGallery';
+import * as Snow from './js/efects/snow';
+  const snow = new Snow.default({
+  id: 'snow',
+  min_size: 1,
+  max_size: 5
+});
+snow.start();
+
+// snow.toggle();
