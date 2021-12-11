3b914b4e56fa0d32b466f995f8f676909762fbe7
Mihail Sinkov <mihailsin88@gmail.com>
2021-12-11 19:20:22 +0200

    adds dynamically evaluated paddings

--- src/js/authForm.js
+++ src/js/authForm.js
@@ -6,7 +6,11 @@ const refs = {
   signInClose: document.querySelector('.sign-in__close'),
   signUpClose: document.querySelector('.sign-up__close'),
 };
-
+const headerHeight = document.querySelector('.header').clientHeight;
+const formWrapper = document.querySelectorAll('.flex-container');
+formWrapper.forEach(element => {
+  element.style.marginTop = `${headerHeight + 20}px`;
+});
 refs.loginTrigger.addEventListener('click', () => {
   refs.signInModal.style.display = 'block';
 });
@@ -19,3 +23,4 @@ refs.signInClose.addEventListener('click', () => {
 refs.signUpClose.addEventListener('click', () => {
   refs.signUpModal.style.display = 'none';
 });
+console.log(headerHeight);
