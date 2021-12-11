c6827b02d6ccc7b5e8ce4583d77561cb5f723033
Irchik512 <chornairchik@gmail.com>
2021-12-11 16:30:25 +0200

    card pravki

--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -25,8 +25,8 @@ const submitHandler = e => {
   renderMoviesOnQuery();
   refs.searchForm.reset();
 };
-refs.searchForm.addEventListener('submit', submitHandler);
-refs.searchIcon.addEventListener('click', submitHandler);
+// refs.searchForm.addEventListener('submit', submitHandler);
+// refs.searchIcon.addEventListener('click', submitHandler);
 export function renderGallery(movies, genres) {
   const markup = movies
     .map(({ id, genre_ids, title, release_date, poster_path }) => {
@@ -37,26 +37,29 @@ export function renderGallery(movies, genres) {
           matchedId.push(genre.name);
         }
       });
+      let cardMatchedId = (matchedId.length < 3) ? (matchedId) : `${matchedId[0]}, ${matchedId[1]}, other`;
       if (poster_path === null) {
         return `<li class="card gallery__item">
       <a href="#" class="card__link" data-id="${id}">
-      <div class ="img-wrapper">
+        <div class="card__wraper-img">
           <img class="card__img" src="https://via.placeholder.com/280x420.png?text=Image+Not+Available" alt="absence of movie's poster">
-          </div>
-          <div class="card__wraper">
-              <h3 class="card__title">${title}</h3>
-              <p class="card__info"><span class="card__info-genre">${matchedId}</span> | <span class="card__info-year">${releaseYear}</span></p>
-          </div>
+        </div>
+        <div class="card__wraper">
+          <h3 class="card__title">${title}</h3>
+          <p class="card__info"><span class="card__info-genre">${cardMatchedId}</span> | <span class="card__info-year">${releaseYear}</span></p>
+        </div>
       </a>
   </li>`;
       }
       return `<li class="card gallery__item">
       <a href="#" class="card__link" data-id="${id}">
+        <div class="card__wraper-img">
           <img class="card__img" src="https://image.tmdb.org/t/p/w780/${poster_path}" alt="movie's poster">
-          <div class="card__wraper">
-              <h3 class="card__title">${title}</h3>
-              <p class="card__info"><span class="card__info-genre">${matchedId}</span> | <span class="card__info-year">${releaseYear}</span></p>
-          </div>
+        </div>
+        <div class="card__wraper">
+          <h3 class="card__title">${title}</h3>
+          <p class="card__info"><span class="card__info-genre">${cardMatchedId}</span> | <span class="card__info-year">${releaseYear}</span></p>
+        </div>
       </a>
   </li>`;
     })
